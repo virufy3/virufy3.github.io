02abc507fda492ca13f2d377206c3c80be60bd5d
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-12-16 23:18:58 -0800

    started gatsby project

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("B:\\Docs\\Programming\\web_dev\\repos\\virufy3.github.io\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("B:\\Docs\\Programming\\web_dev\\repos\\virufy3.github.io\\src\\pages\\index.js")))
+}
+
