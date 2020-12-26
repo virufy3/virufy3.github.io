501fb2abdce81621fc0085dcf291f3a79ff31218
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-12-25 21:23:32 -0800

    seo component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import SEO from "./SEO";
 
 export default ({children}) => {
     return (
         <>
+            <SEO/>
             <p>header</p>
             {children}
             <p>footer</p>
--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {graphql} from "gatsby";
 import GatsbyImage from "gatsby-background-image";
 import {useIntl} from "gatsby-plugin-intl";
+import SEO from "../components/SEO";
 
 // component for displaying team-member info
 const TeamMember = (member) => {
@@ -10,18 +11,16 @@ const TeamMember = (member) => {
     const name = intl.formatMessage({id: `team.${id}.name`});
 
     return (
-        <div class="flex flex-col items-center mx-10 mb-10 md:w-1/3 lg:w-1/4">
+        <div className="flex flex-col items-center mx-10 mb-10 md:w-1/3 lg:w-1/4">
             <a target="_blank" rel="noreferrer" href={intl.formatMessage({id: `team.${id}.mediaLink`})}>
                 <GatsbyImage className="w-64 h-64 mx-auto my-2 rounded-full overflow-hidden team-icons" fluid={image} alt={name} />
             </a>
-            <h5 class="mt-4 text-xl font-semibold text-gray-200">
+            <h5 className="mt-4 text-xl font-semibold text-gray-200">
                 {name}
             </h5>
-            <h6 class="text-lg text-center">{intl.formatMessage({id: `team.${id}.title`})}</h6>
-            <div class="my-2"></div>
-            <p class="w-5/6 text-center">
-                {intl.formatMessage({id: `team.${id}.description`}).split(",,").map(str => <p>{str}</p>)}
-            </p>
+            <h6 className="text-lg text-center">{intl.formatMessage({id: `team.${id}.title`})}</h6>
+            <div className="my-2"></div>
+            {intl.formatMessage({id: `team.${id}.description`}).split(",,").map((str,i) => <p key={i} className="w-5/6 text-center">{str}</p>)}
         </div>
     );
 }
@@ -75,38 +74,39 @@ export default ({data}) => {
 
     return (
         <>
+            <SEO title="Team | Virufy"/>
             <div className="wrapper">
-                <div class="mb-6 md:mb-20">
-                    <h2 class="mb-4 text-4xl font-bold text-center">
+                <div className="mb-6 md:mb-20">
+                    <h2 className="mb-4 text-4xl font-bold text-center">
                         {intl.formatMessage({id: "team.headers.leadership"})}
                     </h2>
                 </div>
-                <section class="flex flex-col flex-wrap mb-10 xl:justify-center md:justify-around md:flex-row">
+                <section className="flex flex-col flex-wrap mb-10 xl:justify-center md:justify-around md:flex-row">
                     {
                         leadership.map(({id, imageName}) => {
                             const teamMemberPic = images.find(({node: {relativePath}}) => {
                                 return relativePath === imageName;
                             }).node.childImageSharp.fluid;
 
-                            return <TeamMember image={teamMemberPic} id={`leadership.${id}`}/>
+                            return <TeamMember key={`${leadership}${id}`} image={teamMemberPic} id={`leadership.${id}`}/>
                         })
                     }
                 </section>
             </div>
-            <div class="wrapper md:mt-12">
-                <div class="mb-6 md:mb-16">
-                    <h2 class="mb-4 text-4xl font-bold text-center">
+            <div className="wrapper md:mt-12">
+                <div className="mb-6 md:mb-16">
+                    <h2 className="mb-4 text-4xl font-bold text-center">
                         {intl.formatMessage({id: "team.headers.advisors"})}
                     </h2>
                 </div>
-                <section class="flex flex-col flex-wrap mb-10 xl:justify-center md:justify-around md:flex-row">
+                <section className="flex flex-col flex-wrap mb-10 xl:justify-center md:justify-around md:flex-row">
                 {
                         advisors.map(({id, imageName}) => {
                             const teamMemberPic = images.find(({node: {relativePath}}) => {
                                 return relativePath === imageName;
                             }).node.childImageSharp.fluid;
 
-                            return <TeamMember image={teamMemberPic} id={`advisors.${id}`}/>
+                            return <TeamMember key={`${advisors}${id}`} image={teamMemberPic} id={`advisors.${id}`}/>
                         })
                     }
                 </section>
