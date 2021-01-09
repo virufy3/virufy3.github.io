23ba0ad965a6d7c25110872efec4c37a862b83c7
lowell1 <29132034+lowell1@users.noreply.github.com>
2021-01-08 17:12:54 -0800

    moved team info to seperate file

---
+++ src/data/team-members.js
@@ -0,0 +1,27 @@
+export const leadership = [
+    { id: 'amil', defaultName: "Amil Khanzada", imageName: 'amil.jpg' },
+    { id: 'gunvant', defaultName: "Gunvant Chaudhari", imageName: 'gunvant.jpg' },
+    { id: 'daria', defaultName: "Daria Butuc", imageName: 'daria.jpg' },
+    { id: 'ravel', defaultName: "Ravel Charles", imageName: 'ravel.jpg' },
+    { id: 'bruna', defaultName: "Bruna Lourenço", imageName: 'bruna.jpg' },
+    { id: 'jonatan', defaultName: "Jonatan Jaskilioff", imageName: 'jonatan.jpg' },
+    { id: 'xhoana', defaultName: "Xhoana Koca", imageName: 'xhoana.jpg' },
+    { id: 'matheus', defaultName: "Matheus Galiza", imageName: 'matheus.jpg' },
+    { id: 'sabrina', defaultName: "Sabrina Shen", imageName: 'sabrina.jpg' },
+    { id: 'sebastian', defaultName: "Sebastian Pedraza", imageName: 'sebastian.jpg' },
+    { id: 'jason', defaultName: "Jason Pareja Jauregui", imageName: 'jason.jpg' },
+    
+  ];
+  
+  export const advisors = [
+    { id: 'kara', defaultName: "Kara Meister, M.D.", imageName: 'kara-meister.jpg' },
+    { id: 'jure', defaultName: "Jure Leskovec", imageName: 'jure.jpg' },
+    { id: 'rafi', defaultName: "Rafi Azim-Khan", imageName: 'rafi.jpg' },
+    { id: 'malvika', defaultName: "Malvika Bawri", imageName: 'malvika.jpg' },
+    { id: 'nathan', defaultName: "Nathan Thomas", imageName: 'nathan.jpg' },
+    { id: 'madhav', defaultName: "Madhav Datt", imageName: 'madhav.jpg' },
+    { id: 'pedro', defaultName: "Pedro Siena", imageName: 'pedro.jpg' },
+    { id: 'tsutomu', defaultName: "Tsutomu Ito", imageName: 'ito.jpg' },
+    { id: 'taisuke', defaultName: "Taisuke Fukuno", imageName: 'taisuke.jpg' },
+    { id: 'richard', defaultName: "Richard Nall", imageName: 'richard.jpg' }
+  ];
