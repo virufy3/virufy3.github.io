b653ce54c86c4e6102bbeeb5e8f1f1b59d7cf597
lowell1 <29132034+lowell1@users.noreply.github.com>
2021-01-12 16:15:40 -0800

    new section in index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Layout from "../components/layout";
+import {useIntl} from "gatsby-plugin-intl";
 
 export default () => {
-    
+    const intl = useIntl();
+
     return (
         <Layout>
-            <div>index</div>;
+            <section>
+                <h1>{intl.formatMessage({id: "index.section1.header"})}</h1>
+                <div className="flex items-center">
+                    <p dangerouslySetInnerHTML={{__html: intl.formatHTMLMessage({id: "index.section1.description"})}}/>
+                    {/* <p>{intl.formatMessage({id: "index.section1.description"})}</p> */}
+                </div>
+            </section>
         </Layout>
     )
 }
