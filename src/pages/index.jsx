2986aeddc432c4c94f9134843ddfcf5b00f748cf
Jacob Daitzman <jdtzmn@gmail.com>
2021-01-14 15:05:28 -0500

    Small touches to linter config

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 export default () => {
-    
-    return (
-        <Layout>
-            <div>index</div>;
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <div>index</div>;
+    </Layout>
+  );
+};
