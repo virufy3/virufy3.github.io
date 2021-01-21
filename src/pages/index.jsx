a6cb91a02e6cecf935beddd5282631003ca457e3
lowell1 <29132034+lowell1@users.noreply.github.com>
2021-01-21 15:29:11 -0800

    added our story section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -41,11 +41,8 @@ export default ({ data }) => {
           </div>
         </div>
       </section>
-      <section>
-        <GatsbyBgImage
-          style={{ backgroundColor: "#393939" }}
-          fluid={images["globe"].childImageSharp.fluid}
-        >
+      <section className="bg-black">
+        <GatsbyBgImage fluid={images["globe"].childImageSharp.fluid}>
           <div className="py-20 w-4/5 flex flex-col mx-auto items-start">
             <h2 className="text-white bold text-3xl mb-4">
               {intl.formatMessage({ id: "index.section2.header" })}
@@ -104,14 +101,11 @@ export default ({ data }) => {
               }}
             >
               <div className="w-full flex">
-                <div
-                  style={{ backgroundColor: "#393939" }}
-                  className="mr-4 text-white test-white text-lg rounded-full w-8 h-8 flex justify-center items-center"
-                >
+                <div className="bg-black mr-4 text-white test-white text-lg rounded-full w-8 h-8 flex justify-center items-center">
                   1
                 </div>
                 <h2
-                  className="text-md text-sm font-bold"
+                  className="text-black text-md text-sm font-bold"
                   style={{ maxWidth: "calc(100% - 3rem)" }}
                 >
                   {intl.formatMessage({
@@ -130,14 +124,11 @@ export default ({ data }) => {
               }}
             >
               <div className="w-full flex">
-                <div
-                  style={{ backgroundColor: "#393939" }}
-                  className="mr-4 text-white test-white text-lg rounded-full w-8 h-8 flex justify-center items-center"
-                >
+                <div className="bg-black mr-4 text-white test-white text-lg rounded-full w-8 h-8 flex justify-center items-center">
                   1
                 </div>
                 <h2
-                  className="text-md text-sm font-bold"
+                  className="text-black text-md text-sm font-bold"
                   style={{ maxWidth: "calc(100% - 3rem)" }}
                 >
                   {intl.formatMessage({
@@ -156,14 +147,11 @@ export default ({ data }) => {
               }}
             >
               <div className="w-full flex ">
-                <div
-                  style={{ backgroundColor: "#393939" }}
-                  className="mr-4 text-white test-white text-lg rounded-full w-8 h-8 flex justify-center items-center"
-                >
+                <div className="bg-black mr-4 text-white test-white text-lg rounded-full w-8 h-8 flex justify-center items-center">
                   1
                 </div>
                 <h2
-                  className="text-md text-sm font-bold"
+                  className="text-black text-md text-sm font-bold"
                   style={{ maxWidth: "calc(100% - 3rem)" }}
                 >
                   {intl.formatMessage({
@@ -206,6 +194,45 @@ export default ({ data }) => {
             </div>
           </div>
         </div>
+      </section>
+      <section
+        className="flex justify-between bg-green-100"
+        style={{ padding: "5rem 10vw" }}
+      >
+        <div className="w-1/2">
+          <h2 className="mb-2 font-bold text-3xl">
+            {intl.formatMessage({ id: "index.section4.header" })}
+          </h2>
+          <p className="mb-8">
+            {intl.formatMessage({ id: "index.section4.pg1" })}
+          </p>
+          <p className="mb-12">
+            {intl.formatMessage({ id: "index.section4.pg2" })}
+          </p>
+          <Link className="text-black bg-white px-8 py-4 mb-16 inline-block font-bold">
+            {intl.formatMessage({ id: "index.section4.knowUsLink" })}
+          </Link>
+          <h2 className="text-black font-bold mb-4">
+            {intl.formatMessage({ id: "index.section4.header2" })}
+          </h2>
+          <div className="flex items-center">
+            <GatsbyImage
+              className="w-16 mr-4"
+              fluid={
+                images["stanford-h4r-logo transparent"].childImageSharp.fluid
+              }
+            />
+            <GatsbyImage
+              className="w-20"
+              fluid={images["OYW_blue_rgb"].childImageSharp.fluid}
+            />
+          </div>
+        </div>
+        <GatsbyImage
+          className="w-2/5"
+          fluid={images["Virufighters"].childImageSharp.fluid}
+          imgStyle={{ objectFit: "fill" }}
+        />
       </section>
     </Layout>
   );
