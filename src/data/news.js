d33a9f37a3fc3aed89c6969ba2513d0312511977
hwyj3464 <hwyj3464@gmail.com>
2021-01-30 22:42:56 -0500

    Initial try

---
+++ src/data/news.js
@@ -0,0 +1,47 @@
+export const news = [
+  {
+    mediaLink:
+      "https://www.expreso.com.pe/actualidad/virufy-plataforma-que-permite-detectar-si-estamos-contagiados-con-covid-19/",
+    id: "section2",
+    imageName: "Expreso.png",
+  },
+  {
+    mediaLink:
+      "https://oywj.org/ambassadors-in-action/2020/05/oywj-ambassador-targets-covid-19-using-ai/",
+    id: "section3",
+    imageName: "OYW Japan 2.png",
+  },
+  {
+    mediaLink:
+      "https://www.oneyoungworld.com/north-american-ambassadors-frontlines-covid-19-response",
+    id: "section4",
+    imageName: "OYW_white_rgb.png",
+  },
+  {
+    mediaLink:
+      "https://www.oneyoungworld.com/news-item/july-2020-ambassadors-month",
+    id: "section5",
+    imageName: "OYW_white_rgb.png",
+  },
+  {
+    mediaLink:
+      "https://www.oneyoungworld.com/news-item/thousands-attend-oyws-global-digital-events-july-recap-what-youve-missed",
+    id: "section6",
+    imageName: "OYW_white_rgb.png",
+  },
+  {
+    mediaLink: "https://www.sdgscaucus.com/speakers.html",
+    id: "section7",
+    imageName: "OYW_white_rgb.png",
+  },
+  {
+    mediaLink: "https://youtu.be/UZU3rPTEPfU",
+    id: "section8",
+    imageName: "virufyPresentation.png",
+  },
+  {
+    mediaLink: "https://health-sounds.cl.cam.ac.uk/workshop20/short-talks.html",
+    id: "section9",
+    imageName: "soundsOfCovid.png",
+  },
+];
--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -4,6 +4,7 @@ import { useIntl } from "gatsby-plugin-intl";
 import { Link, graphql } from "gatsby";
 import SEO from "../components/SEO";
 import Img from "gatsby-image";
+import { news } from "../data/news";
 
 export const query = graphql`
   {
@@ -22,282 +23,62 @@ export const query = graphql`
   }
 `;
 
+const NewsList = (props) => {
+  const intl = useIntl();
+
+  return (
+    <section className="flex flex-col flex-wrap mb-10 xl:justify-center md:justify-around md:flex-row mb-24">
+      <div className="w-1/2">
+        <Img className="w-3/5" fluid={image} />
+      </div>
+      <div className="w-1/2">
+        <p className="mb-8">
+          {intl.formatMessage({ id: `news.${role}.${id}.date` })}
+        </p>
+        <h3 className="mb-8 font-bold text-3xl">
+          {intl.formatMessage({ id: `news.${role}.${id}.title` })}
+        </h3>
+        <p>{intl.formatMessage({ id: `news.${role}.${id}.name` })}</p>
+        <p className="mb-8">
+          {intl.formatMessage({ id: `news.${role}.${id}.location` })}
+        </p>
+
+        <a target="_blank" rel="noreferrer" href={mediaLink}>
+          <u>{intl.formatMessage({ id: `news.${role}.${id}.readMoreLink` })}</u>
+        </a>
+      </div>
+    </section>
+  );
+};
+
 export default ({ data }) => {
   const images = data.allFile.edges;
   const intl = useIntl();
 
-  const findImage = useCallback(
-    (imageName, ext = ".png") => {
-      return images.find(({ node: { relativePath } }) => {
-        return relativePath === `${imageName}${ext}`;
-      }).node.childImageSharp.fluid;
-    },
-    [images]
-  );
-
   return (
     <Layout>
       <SEO title="News | Virufy" />
-      <div className="wrapper">
+      <div className="wrapper my-10 md:my-20">
         <h2 className="mb-8 font-bold text-3xl">
-          {intl.formatMessage({ id: "news.section1.header" })}
+          {intl.formatMessage({ id: "news.headers.header" })}
         </h2>
-
-        <section className="flex justify-between mb-24">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("Expreso")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section2.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section2.title" })}
-            </h3>
-            <p>{intl.formatMessage({ id: "news.section2.name" })}</p>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section2.location" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={
-                "https://www.expreso.com.pe/actualidad/virufy-plataforma-que-permite-detectar-si-estamos-contagiados-con-covid-19/"
-              }
-            >
-              <u>{intl.formatMessage({ id: "news.section2.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
-
-        <section className="flex justify-between mb-24">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("OYW Japan 2")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section3.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section3.title" })}
-            </h3>
-            <p>{intl.formatMessage({ id: "news.section3.name" })}</p>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section3.location" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={
-                "https://oywj.org/ambassadors-in-action/2020/05/oywj-ambassador-targets-covid-19-using-ai/"
-              }
-            >
-              <u>{intl.formatMessage({ id: "news.section3.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
-
-        <section className="flex justify-between mb-24">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("OYW_white_rgb")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section4.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section4.title" })}
-            </h3>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section4.name" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={
-                "https://www.oneyoungworld.com/north-american-ambassadors-frontlines-covid-19-response"
-              }
-            >
-              <u>{intl.formatMessage({ id: "news.section4.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
-
-        <section className="flex justify-between mb-24">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("OYW_white_rgb")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section5.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section5.title" })}
-            </h3>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section5.name" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={
-                "https://www.oneyoungworld.com/news-item/july-2020-ambassadors-month"
-              }
-            >
-              <u>{intl.formatMessage({ id: "news.section5.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
-
-        <section className="flex justify-between mb-24">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("OYW_white_rgb")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section6.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section6.title" })}
-            </h3>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section6.name" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={
-                "https://www.oneyoungworld.com/news-item/thousands-attend-oyws-global-digital-events-july-recap-what-youve-missed"
-              }
-            >
-              <u>{intl.formatMessage({ id: "news.section6.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
-
-        <section className="flex justify-between mb-24">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("OYW_white_rgb")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section7.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section7.title" })}
-            </h3>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section7.name" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={"https://www.sdgscaucus.com/speakers.html"}
-            >
-              <u>{intl.formatMessage({ id: "news.section7.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
-
-        <section className="flex justify-between mb-24">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("virufyPresentation")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section8.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section8.title" })}
-            </h3>
-            <p>{intl.formatMessage({ id: "news.section8.name" })}</p>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section8.location" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={"https://youtu.be/UZU3rPTEPfU"}
-            >
-              <u>{intl.formatMessage({ id: "news.section8.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
-
-        <section className="flex justify-between mb-8">
-          <div className="w-1/2">
-            <Img
-              className="w-3/5"
-              fluid={findImage("soundsOfCovid")}
-              imgStyle={{ objectFit: "contain" }}
-              aria-hidden
-            />
-          </div>
-          <div className="w-1/2">
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section9.date" })}
-            </p>
-            <h3 className="mb-8 font-bold text-3xl">
-              {intl.formatMessage({ id: "news.section9.title" })}
-            </h3>
-            <p className="mb-8">
-              {intl.formatMessage({ id: "news.section9.name" })}
-            </p>
-
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={
-                "https://health-sounds.cl.cam.ac.uk/workshop20/short-talks.html"
-              }
-            >
-              <u>{intl.formatMessage({ id: "news.section9.readMoreLink" })}</u>
-            </a>
-          </div>
-        </section>
       </div>
+
+      {news.map((item) => {
+        const NewsPic = images.find(({ node: { relativePath } }) => {
+          return relativePath === item.imageName;
+        }).node.childImageSharp.fluid;
+
+        return (
+          <NewsList
+            key={`${news}${item.id}`}
+            role="news"
+            image={NewsPic}
+            person={item}
+          />
+        );
+        // return <TeamMember key={`${leadership}${id}`} image={teamMemberPic} id={`leadership.${id}`}/>
+      })}
     </Layout>
   );
 };
