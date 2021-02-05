f9b4ad1a03368d5980b8f855a414cdc2a0db37ec
lowell1 <29132034+lowell1@users.noreply.github.com>
2021-02-04 22:12:09 -0800

    changed our values to table

--- src/components/team/ValuesSection.jsx
+++ src/components/team/ValuesSection.jsx
@@ -51,92 +51,124 @@ export default ({ images }) => {
         />
       </div>
       {panelOpen && (
-        <div className="flex justify-between lg:pb-32">
-          <div className="flex flex-col items-baseline">
-            <h2 className="font-bold mt-8 mb-4 text-lg">
-              {intl.formatMessage({ id: "team.valuesSection.values.header" })}
-            </h2>
-            <div className="flex mb-4">
-              <h3 className="font-bold w-40 flex-shrink-0 mb-4">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.transform.header",
-                })}
-              </h3>
-              <p className="w-full">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.transform.pg",
-                })}
-              </p>
-            </div>
-            <div className="flex mb-4">
-              <h3 className="font-bold w-40 flex-shrink-0 mb-4">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.world.header",
-                })}
-              </h3>
-              <p className="w-full">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.world.pg",
-                })}
-              </p>
-            </div>
-            <div className="flex mb-4">
-              <h3 className="font-bold w-40 flex-shrink-0 mb-4">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.free.header",
-                })}
-              </h3>
-              <p className="w-full">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.free.pg",
-                })}
-              </p>
-            </div>
-            <div className="flex mb-4">
-              <h3 className="font-bold w-40 flex-shrink-0 mb-4">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.health.header",
-                })}
-              </h3>
-              <p className="w-full">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.health.pg",
-                })}
-              </p>
-            </div>
-            <div className="flex mb-4">
-              <h3 className="font-bold w-40 flex-shrink-0 mb-4">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.oneTap.header",
-                })}
-              </h3>
-              <p className="w-full">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.oneTap.pg",
-                })}
-              </p>
-            </div>
-            <div className="flex">
-              <h3 className="font-bold w-40 flex-shrink-0">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.mobile.header",
-                })}
-              </h3>
-              <p className="w-full">
-                {intl.formatMessage({
-                  id: "team.valuesSection.values.mobile.pg",
-                })}
-              </p>
+        <>
+          <h2 className="font-bold mt-8 mb-4 text-lg">
+            {intl.formatMessage({ id: "team.valuesSection.values.header" })}
+          </h2>
+          <div className="flex lg:pb-32">
+            <table>
+              {["transform", "world", "free", "health", "oneTap", "mobile"].map(
+                (rowName) => (
+                  <tr>
+                    <td className="align-top pr-4 font-bold">
+                      {intl.formatMessage({
+                        id: `team.valuesSection.values.${rowName}.header`,
+                      })}
+                    </td>
+                    <td className="align-top pb-4">
+                      {intl.formatMessage({
+                        id: `team.valuesSection.values.${rowName}.pg`,
+                      })}
+                    </td>
+                  </tr>
+                )
+              )}
+            </table>
+            <div className="lg:flex items-end hidden">
+              <GatsbyImage
+                fluid={images["people_looking_at_phone"].childImageSharp.fluid}
+                imgStyle={{ objectFit: "fill" }}
+                className="w-60 h-60  flex self-end transform translate-y-32  "
+              />
             </div>
           </div>
-          <div className="lg:flex items-end hidden">
-            <GatsbyImage
-              fluid={images["people_looking_at_phone"].childImageSharp.fluid}
-              imgStyle={{ objectFit: "fill" }}
-              className="w-60 h-60  flex self-end transform translate-y-32  "
-            />
-          </div>
-        </div>
+        </>
+        // <div className="flex justify-between lg:pb-32">
+        //   <div className="flex flex-col items-baseline">
+        //     <h2 className="font-bold mt-8 mb-4 text-lg">
+        //       {intl.formatMessage({ id: "team.valuesSection.values.header" })}
+        //     </h2>
+        //     <div className="flex mb-4">
+        //       <h3 className="font-bold w-40 flex-shrink-0 mb-4">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.transform.header",
+        //         })}
+        //       </h3>
+        //       <p className="w-full">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.transform.pg",
+        //         })}
+        //       </p>
+        //     </div>
+        //     <div className="flex mb-4">
+        //       <h3 className="font-bold w-40 flex-shrink-0 mb-4">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.world.header",
+        //         })}
+        //       </h3>
+        //       <p className="w-full">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.world.pg",
+        //         })}
+        //       </p>
+        //     </div>
+        //     <div className="flex mb-4">
+        //       <h3 className="font-bold w-40 flex-shrink-0 mb-4">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.free.header",
+        //         })}
+        //       </h3>
+        //       <p className="w-full">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.free.pg",
+        //         })}
+        //       </p>
+        //     </div>
+        //     <div className="flex mb-4">
+        //       <h3 className="font-bold w-40 flex-shrink-0 mb-4">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.health.header",
+        //         })}
+        //       </h3>
+        //       <p className="w-full">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.health.pg",
+        //         })}
+        //       </p>
+        //     </div>
+        //     <div className="flex mb-4">
+        //       <h3 className="font-bold w-40 flex-shrink-0 mb-4">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.oneTap.header",
+        //         })}
+        //       </h3>
+        //       <p className="w-full">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.oneTap.pg",
+        //         })}
+        //       </p>
+        //     </div>
+        //     <div className="flex">
+        //       <h3 className="font-bold w-40 flex-shrink-0">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.mobile.header",
+        //         })}
+        //       </h3>
+        //       <p className="w-full">
+        //         {intl.formatMessage({
+        //           id: "team.valuesSection.values.mobile.pg",
+        //         })}
+        //       </p>
+        //     </div>
+        //   </div>
+        //   <div className="lg:flex items-end hidden">
+        //     <GatsbyImage
+        //       fluid={images["people_looking_at_phone"].childImageSharp.fluid}
+        //       imgStyle={{ objectFit: "fill" }}
+        //       className="w-60 h-60  flex self-end transform translate-y-32  "
+        //     />
+        //   </div>
+        // </div>
       )}
     </section>
   );
