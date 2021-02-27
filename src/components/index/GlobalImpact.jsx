0dcdb2cdaa308a27b71de1da57fa6891d393329d
hwyj3464 <hwyj3464@gmail.com>
2021-02-26 21:14:02 -0500

    update index page

--- src/components/index/GlobalImpact.jsx
+++ src/components/index/GlobalImpact.jsx
@@ -8,22 +8,24 @@ export default ({ images }) => {
 
   return (
     <section className="bg-black">
-      <GatsbyBgImage fluid={images["globe"].childImageSharp.fluid}>
-        <div className="py-20 w-4/5 flex flex-col mx-auto items-start">
-          <h2 className="text-white bold text-3xl mb-4">
-            {intl.formatMessage({ id: "index.section2.header" })}
-          </h2>
-          <p
-            className="text-white mb-4"
-            dangerouslySetInnerHTML={{
-              __html: intl.formatMessage({ id: "index.section2.pg1" }),
-            }}
-          />
-          <p className="text-white">
-            {intl.formatMessage({ id: "index.section2.pg2" })}
-          </p>
+      <GatsbyBgImage fluid={images["global"].childImageSharp.fluid}>
+        <div className="py-20 w-4/5 flex mx-auto flex-col items-start">
+          <div className="mx-auto">
+            <h2 className="text-white bold text-3xl mb-4 mx-auto">
+              {intl.formatMessage({ id: "index.section2.header" })}
+            </h2>
+            <p
+              className="text-white mb-4"
+              dangerouslySetInnerHTML={{
+                __html: intl.formatMessage({ id: "index.section2.pg1" }),
+              }}
+            />
+            <p className="text-white">
+              {intl.formatMessage({ id: "index.section2.pg2" })}
+            </p>
+          </div>
           <Link
-            className="no-underline bg-white text-black mx-auto my-8 px-8 py-4 font-bold"
+            className="no-underline bg-white text-black mx-auto my-10 px-8 py-4 font-bold"
             target="_blank"
             to={"/app"}
           >
