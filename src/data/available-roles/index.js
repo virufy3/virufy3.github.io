4e4254d40dc2f2547ce68e541c21c755337cc868
cheshireoctopus <chandlermoisen@gmail.com>
2021-03-03 21:50:17 -0700

    Renders roles

---
+++ src/data/available-roles/index.js
@@ -0,0 +1,26 @@
+export const roles = [
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
+    location: "Remote",
+    title: "Clinical Research Coordinator",
+    url:
+      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
+    location: "Remote",
+    title: "Clinical Research Coordinator",
+    url:
+      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
+    location: "Remote",
+    title: "Clinical Research Coordinator",
+    url:
+      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
+  },
+];
--- src/pages/available-roles.js
+++ src/pages/available-roles.js
@@ -2,13 +2,44 @@ import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import HeaderImage from "../images/available-roles-page/header.svg";
+import { roles } from "../data/available-roles";
+
+const Role = ({ description, location, title, url }) => (
+  <div
+    className="grid items-center gap-14 bg-white rounded-xl px-11 py-6 mb-14"
+    style={{ gridTemplateColumns: "4fr max-content max-content" }}
+  >
+    <div className="min-w-0">
+      <p className="font-bold font-heading">{title}</p>
+
+      <p className="font-heading whitespace-nowrap overflow-hidden overflow-ellipsis">
+        {description}
+      </p>
+    </div>
+
+    <div className="mr-4">
+      <p className="font-bold font-heading">Location</p>
+
+      <p className="font-heading">{location}</p>
+    </div>
+
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="font-heading text-sm no-underline bg-primary text-white uppercase font-bold px-8 py-4 inline-block rounded-full"
+    >
+      Read More
+    </a>
+  </div>
+);
 
 export default function AvailableRoles() {
   return (
     <Layout className="bg-green-200">
       <SEO title="Available Roles | Virufy" />
 
-      <div className="relative">
+      <div className="relative mb-28">
         <div className="pt-20 pl-20">
           <h1 className="text-6xl max-w-2xl mb-8">Join the Virufighter team</h1>
 
@@ -25,6 +56,14 @@ export default function AvailableRoles() {
         </div>
 
         <HeaderImage className="absolute top-0 -right-12" />
+      </div>
+
+      <div className="px-40">
+        <h2 className="text-3xl mb-11 font-heading">Job Openings</h2>
+
+        {roles.map((role) => (
+          <Role key={role.title} {...role} />
+        ))}
       </div>
     </Layout>
   );
