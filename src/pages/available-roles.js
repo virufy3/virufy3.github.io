c7cbff75d5967518435721c9331b4e0def89bf74
cheshireoctopus <chandlermoisen@gmail.com>
2021-03-05 08:15:06 -0700

    Adds responsive styling to Available Roles page

--- src/pages/available-roles.js
+++ src/pages/available-roles.js
@@ -6,18 +6,20 @@ import { roles } from "../data/available-roles";
 
 const Role = ({ description, location, title, url }) => (
   <div
-    className="grid items-center gap-14 bg-white rounded-xl px-11 py-6 mb-14"
+    className="flex flex-col md:grid items-center gap-6 md:gap-14 bg-white rounded-xl px-6 md:px-11 py-6 mb-8 md:mb-14"
     style={{ gridTemplateColumns: "4fr max-content max-content" }}
   >
     <div className="min-w-0">
-      <p className="font-bold font-heading">{title}</p>
+      <p className="font-bold font-heading mb-3 md:mb-0 text-sm md:text-md">
+        {title}
+      </p>
 
-      <p className="font-heading whitespace-nowrap overflow-hidden overflow-ellipsis">
+      <p className="font-heading md:whitespace-nowrap overflow-hidden overflow-ellipsis text-sm md:text-md">
         {description}
       </p>
     </div>
 
-    <div className="mr-4">
+    <div className="hidden md:block mr-4">
       <p className="font-bold font-heading">Location</p>
 
       <p className="font-heading">{location}</p>
@@ -27,7 +29,7 @@ const Role = ({ description, location, title, url }) => (
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="font-heading text-sm no-underline bg-primary text-white uppercase font-bold px-8 py-4 inline-block rounded-full"
+      className="block w-full text-center font-heading text-sm no-underline bg-primary text-white uppercase font-bold px-8 py-4 inline-block rounded-full text-sm md:text-md"
     >
       Read More
     </a>
@@ -39,27 +41,31 @@ export default function AvailableRoles() {
     <Layout className="bg-green-200 pb-1">
       <SEO title="Available Roles | Virufy" />
 
-      <div className="relative mb-28">
-        <div className="pt-20 pl-20">
-          <h1 className="text-6xl max-w-2xl mb-8">Join the Virufighter team</h1>
+      <div className="relative mb-11 md:mb-28">
+        <div className="px-8 pt-10 md:pt-20 md:pl-20">
+          <h1 className="text-2xl md:text-6xl max-w-2xl mb-5 md:mb-8">
+            Join the Virufighter team
+          </h1>
 
-          <p className="mb-8 font-heading max-w-2xl">
+          <p className="mb-8 font-heading max-w-2xl text-sm md:text-md">
             Our youth-led team fosters a positive learning environment with a
             focus on mentorship to allow volunteers to hone their skills to make
             worldwide impact. Our volunteers span more than 15 countries,
             contributing their unique perspectives.
           </p>
 
-          <p className="font-bold font-heading">
+          <p className="font-bold font-heading text-sm md:text-md">
             Got what it takes? Check out opportunities below!
           </p>
         </div>
 
-        <HeaderImage className="absolute top-0 -right-12" />
+        <HeaderImage className="absolute top-0 -right-12 hidden xl:block" />
       </div>
 
-      <div className="px-40">
-        <h2 className="text-3xl mb-11 font-heading">Job Openings</h2>
+      <div className="px-8 xl:px-40">
+        <h2 className="text-lg md:text-3xl mb-6 md:mb-11 font-heading">
+          Job Openings
+        </h2>
 
         {roles.map((role) => (
           <Role key={role.title} {...role} />
