2c6ad0f2eacb1947149cae3b37b70d392ac5cb5b
Yoonju Hwang <hwyj3464@users.noreply.github.com>
2021-03-13 11:51:55 -0500

    update roles

--- src/data/available-roles/index.js
+++ src/data/available-roles/index.js
@@ -1,27 +1,11 @@
 export const roles = [
   {
     description:
-      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
+      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the Virufy development team, define the product roadmap, manage development progress and align with stakeholders.",
     location: "Remote",
-    title: "Clinical Research Coordinator",
+    title: "Project / Product Manager",
     url:
       "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
-  },
-  {
-    description:
-      "In order to advance Virufy’s technology, we need as much clinical data as possible from patients of multiple backgrounds and cultural identities in Latin America.",
-    location: "Remote",
-    title: "Clinical Outreach Member (Latin America)",
-    url:
-      "https://docs.google.com/document/d/1EZUFq9Y9xLFIaAB9NMHrICgyHkOyMcoUo9KzB99Qe4E/edit",
-  },
-  {
-    description:
-      "Seeking an individual to contribute on a part-time volunteer basis as a Jack/Jill-of-all-trades!",
-    location: "Remote",
-    title: "Swiss Army Knife",
-    url:
-      "https://docs.google.com/document/d/1PaQroE6uIBFroMP05UKTLXc4ABlS1mqwWm_aXlwdxQw/edit",
   },
   {
     description:
@@ -41,11 +25,43 @@ export const roles = [
   },
   {
     description:
+      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for drafting grant proposals on Virufy’s behalf to secure funding from foundations, corporations, private donors, and fiscal nonprofit sponsors. We are looking for a strong writer to create a compelling narrative around Virufy’s mission and business plan, so that our group will be competitive among thousands of other grant apps.",
+    location: "Remote",
+    title: "Content Writer / Grant Writer",
+    url:
+      "https://docs.google.com/document/u/3/d/191ZZPxEQs-eP_pdzqcpsOcEaFrQedj4kf14kHxfKNFI/edit",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the management and development team to help manage the internal data privacy concerns, potentially under mentorship from senior licensed lawyers.",
+    location: "Remote",
+    title: "Infosec Engineer",
+    url:
+      "https://docs.google.com/document/d/13aFvZWmW3xBsO-IVvCdt2dhfcZvFFy1aBsMd1BZ52Ak",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis as a Jack/Jill-of-all-trades!",
+    location: "Remote",
+    title: "Swiss Army Knife",
+    url:
+      "https://docs.google.com/document/d/1PaQroE6uIBFroMP05UKTLXc4ABlS1mqwWm_aXlwdxQw/edit",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with Virufy management and globally-dispersed legal teams to help manage data privacy concerns for their local jurisdictions.",
+    location: "Remote",
+    title: "Data Privacy Lawyer",
+    url:
+      "https://docs.google.com/document/d/1onEOkzwo7rEl2IWcDWIBGzl8_RrJYdiWbiGiUApcnrc",
+  },
+  {
+    description:
       "Seeking an individual to lead Virufy’s operations across Indonesia.",
     location: "Remote",
-    title: "Indonesia Country Director / Head of Mission",
+    title: "Indonesia Country Director  / Head of Mission",
     url:
-      "https://docs.google.com/document/d/10hBe2BsByjSgOGHiHZWjSex9AniwxDCis8jZeoCFOHU",
+      "https://docs.google.com/document/u/3/d/10hBe2BsByjSgOGHiHZWjSex9AniwxDCis8jZeoCFOHU/edit",
   },
   {
     description:
@@ -65,22 +81,6 @@ export const roles = [
   },
   {
     description:
-      "We are seeking a part-time volunteer to draft content on Virufy’s behalf to secure funding from foundations, corporations, private donors, and fiscal nonprofit sponsors.",
-    location: "Remote",
-    title: "Content Writer",
-    url:
-      "https://docs.google.com/document/d/191ZZPxEQs-eP_pdzqcpsOcEaFrQedj4kf14kHxfKNFI",
-  },
-  {
-    description:
-      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for drafting grant proposals on Virufy’s behalf to secure funding from foundations, corporations, private donors, and fiscal nonprofit sponsors.",
-    location: "Remote",
-    title: "Grant Writer",
-    url:
-      "https://docs.google.com/document/d/1w3tI6neqQx7_EUv7eLT45Oo0F9qHO5mOs4o8MxSRvbw/",
-  },
-  {
-    description:
       "Seeking a part-time, volunteer film maker who is passionate about editing digital content and creating graphics/illustrations for outreach and marketing purposes.",
     location: "Remote",
     title: "Film Maker",
@@ -89,19 +89,19 @@ export const roles = [
   },
   {
     description:
+      "Seeking an individual to contribute on a part-time, volunteer basis to evaluate and screen potential candidates to fill vacant volunteering opportunities at Virufy. A key element of this position is the ability to attract highly-competitive and best-fit candidates for the job. Success in this role will be determined by providing an increased number of qualified and passionate candidates and retention rates.",
+    location: "Remote",
+    title: "Talent Sourcer",
+    url:
+      "https://docs.google.com/document/d/1umbOcHv3ZAMHIRx9XwWyAcFrdMlpdiimwJmwzKh_Jr0/edit",
+  },
+  {
+    description:
       "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the management and development team to help manage the overall data privacy concerns internally, potentially under mentorship from senior licensed lawyers.",
     location: "Remote",
     title: "Data Privacy Manager",
     url:
       "https://docs.google.com/document/d/1v86sEXJYMiWLoFIqC12QCnqbJu2KIOKWlbs35aC7Acc",
-  },
-  {
-    description:
-      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with Virufy management and globally-dispersed legal teams to help manage data privacy concerns for their local jurisdictions.",
-    location: "Remote",
-    title: "Data Privacy Lawyer",
-    url:
-      "https://docs.google.com/document/d/1onEOkzwo7rEl2IWcDWIBGzl8_RrJYdiWbiGiUApcnrc",
   },
   {
     description:
@@ -121,18 +121,18 @@ export const roles = [
   },
   {
     description:
-      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the management and development team to help manage the internal data privacy concerns, potentially under mentorship from senior licensed lawyers.",
+      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
     location: "Remote",
-    title: "Infosec Engineer",
+    title: "Clinical Research Coordinator",
     url:
-      "https://docs.google.com/document/d/13aFvZWmW3xBsO-IVvCdt2dhfcZvFFy1aBsMd1BZ52Ak",
+      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
   },
   {
     description:
-      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the Virufy development team, define the product roadmap, manage development progress and align with stakeholders.",
+      "In order to advance Virufy’s technology, we need as much clinical data as possible from patients of multiple backgrounds and cultural identities in Latin America.",
     location: "Remote",
-    title: "Project / Product Manager",
+    title: "Clinical Outreach Member (Latin America)",
     url:
-      "https://docs.google.com/document/d/17ISJC3yIhp7FAzLRYDDueC5Kr8sqCIUKndgvcmqBkow",
+      "https://docs.google.com/document/d/1EZUFq9Y9xLFIaAB9NMHrICgyHkOyMcoUo9KzB99Qe4E/edit",
   },
 ];
