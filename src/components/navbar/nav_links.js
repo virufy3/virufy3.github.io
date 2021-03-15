f372dfc17ea67722a8f6a52de7bcb71bbea8c9fd
Yoonju Hwang <hwyj3464@users.noreply.github.com>
2021-03-15 18:35:55 -0400

    update nav

--- src/components/navbar/nav_links.js
+++ src/components/navbar/nav_links.js
@@ -6,6 +6,7 @@ export default [
     path: "/our-approach",
     dropDownLinks: [
       { intlId: "nav.techBehind", sectionId: "tech-behind" },
+      { intlId: "nav.openData", sectionId: "open-data" },
       { intlId: "nav.faqs", sectionId: "faq" },
     ],
   },
