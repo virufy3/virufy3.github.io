8551eb0dc5e3e76e5d0ef1044c6d837fcd35f3cb
Freddy Lemus <freddylemus@orioninfiniti.com>
2021-03-21 11:03:44 -0500

    Website FAQ comment before

--- src/data/our-approach/faq.js
+++ src/data/our-approach/faq.js
@@ -73,4 +73,30 @@ export default [
       "otherDetails.WhatIfi",
     ],
   },
+  // {
+  //   categoryId: "missionCategory",
+  //   questionIds: ["nonprofit", "postVaccine", "postControl"],
+  // },
+  // {
+  //   categoryId: "detectionCategory",
+  //   questionIds: ["distinguishable", "diagnosis", "performance"],
+  // },
+  // {
+  //   categoryId: "privacyCategory",
+  //   questionIds: [
+  //     "dataStorage",
+  //     "privacyThreats",
+  //     "privacyPolicy",
+  //     "thirdParties",
+  //     "validTechnology",
+  //   ],
+  // },
+  // {
+  //   categoryId: "financeCategory",
+  //   questionIds: ["financialContribution", "appCost"],
+  // },
+  // {
+  //   categoryId: "generalCategory",
+  //   questionIds: ["moreQuestions"],
+  // },
 ];
