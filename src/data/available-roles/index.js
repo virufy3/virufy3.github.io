ed5ba42e87547caf8a84563d91e44b6fed18e578
Yoonju Hwang <hwyj3464@users.noreply.github.com>
2021-04-06 22:49:29 -0400

    update roles

--- src/data/available-roles/index.js
+++ src/data/available-roles/index.js
@@ -1,21 +1,12 @@
 export const roles = [
   {
     description:
-      "Seeking an individual to volunteer on a part-time basis to help execute full-scale marketing campaigns to manage our social media platforms and to engage influencers and the press.",
-    location: "Remote",
-    title: "Virufy Outreach Ambassador",
-    url:
-      "https://docs.google.com/document/u/4/d/1CeqhlcFkeNHQh8NZ1UQGxvjQT-5U-j0MO6tZHLzHyr0/edit",
-  },
-  {
-    description:
       "Seeking an individual to contribute on a part-time volunteer basis to raise funds for our global marketing and data crowdsourcing campaigns to continue operating.",
     location: "Remote",
     title: "Fundraising Team Volunteer",
     url:
       "https://docs.google.com/document/d/1wR0Cn30ThlSa5iCc-bHgbx0_6dhmNsFU2ptVZQxmkRQ/edit",
   },
-
   {
     description:
       "Seeking an individual to contribute on a part-time volunteer basis and lead the North America ambassador team on the fundraising end to raise money for our global marketing and data crowdsourcing campaigns.",
@@ -23,6 +14,14 @@ export const roles = [
     title: "Fundraising Coordinator",
     url:
       "https://docs.google.com/document/d/1OG3dc5acRJjEkys8kVp-NScttaNziQ1mdBWUIm6cnlY/edit",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
+    location: "Remote",
+    title: "Clinical Research Coordinator",
+    url:
+      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
   },
   {
     description:
@@ -50,28 +49,26 @@ export const roles = [
   },
   {
     description:
-      "Seeking an individual to serve as the first point of contact within Virufy for all HR-related inquiries.",
-    location: "Remote",
-    title: "Human Resources Administrator",
-    url:
-      "https://docs.google.com/document/d/191xws4_-ocyyMS9zTP0MEjsd2oXjGJqfNeVjyTaqepk",
-  },
-
-  {
-    description:
-      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the management and development team to help manage the internal data privacy concerns, potentially under mentorship from senior licensed lawyers.",
-    location: "Remote",
-    title: "Infosec Engineer",
-    url:
-      "https://docs.google.com/document/d/13aFvZWmW3xBsO-IVvCdt2dhfcZvFFy1aBsMd1BZ52Ak",
-  },
-  {
-    description:
       "Seeking an individual to volunteer on a part-time basis to help us develop a full-scale marketing campaign to manage our social media platforms and to engage influencers and the press.",
     location: "Remote",
     title: "Global Marketing Manager",
     url:
       "https://docs.google.com/document/d/1UTIg6rukg6B8pvLGfrB2t5COUFb5kug6x8YUsf-8WxY/",
+  },
+  {
+    description:
+      "Seeking an individual to volunteer on a part-time basis to help execute full-scale marketing campaigns to manage our social media platforms and to engage influencers and the press.",
+    location: "Remote",
+    title: "Virufy Outreach Ambassador",
+    url: "https://virufy.org/apply",
+  },
+  {
+    description:
+      "In order to advance Virufy’s technology, we need as much clinical data as possible from patients of multiple backgrounds and cultural identities in Latin America.",
+    location: "Remote",
+    title: "Clinical Outreach Member (Latin America)",
+    url:
+      "https://docs.google.com/document/d/1EZUFq9Y9xLFIaAB9NMHrICgyHkOyMcoUo9KzB99Qe4E/edit",
   },
   {
     description:
@@ -81,7 +78,6 @@ export const roles = [
     url:
       "https://docs.google.com/document/d/124lK1uwh7fgaMD-J974jbLXO9iQEDl7pFiIbxigCcyI",
   },
-
   {
     description:
       "Seeking a part-time, volunteer film maker who is passionate about editing digital content and creating graphics/illustrations for outreach and marketing purposes.",
@@ -89,14 +85,6 @@ export const roles = [
     title: "Film Maker",
     url:
       "https://docs.google.com/document/d/1bgIGXD_LUUrbtszgHlOhdUnbojWfmMlSXNwnI1naDTc",
-  },
-  {
-    description:
-      "Seeking an individual to contribute on a part-time, volunteer basis to evaluate and screen potential candidates to fill vacant volunteering opportunities at Virufy. A key element of this position is the ability to attract highly-competitive and best-fit candidates for the job. Success in this role will be determined by providing an increased number of qualified and passionate candidates and retention rates.",
-    location: "Remote",
-    title: "Talent Sourcer",
-    url:
-      "https://docs.google.com/document/d/1umbOcHv3ZAMHIRx9XwWyAcFrdMlpdiimwJmwzKh_Jr0/edit",
   },
   {
     description:
@@ -108,14 +96,6 @@ export const roles = [
   },
   {
     description:
-      "Our team of part-time volunteers works with machine learning and signal analysis for audio analysis in a medical setting. We welcome individuals with professional experience in these domains to apply. The candidate will be immersed in a collaborative environment alongside AI engineers and mentors.",
-    location: "Remote",
-    title: "Backend Engineer(Python)",
-    url:
-      "https://docs.google.com/document/d/1S6ywprk2DycFLA8jyv9_2DxXiT4t7eDptymh1VaP7eY/edit",
-  },
-  {
-    description:
       "Seeking a part-time volunteer to work with machine learning, deep learning, and signal processing for audio analysis in a medical setting. We welcome students and professionals with experience in these domains. The candidate will be immersed in a highly collaborative environment alongside senior AI engineers and mentors.",
     location: "Remote",
     title: "Full Stack Engineer",
@@ -124,18 +104,10 @@ export const roles = [
   },
   {
     description:
-      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
+      "Seeking a part-time volunteer to work with machine learning, deep learning, and signal processing for audio analysis in a medical setting. We welcome students and professionals with experience in these domains. The candidate will be immersed in a highly collaborative environment alongside senior AI engineers and mentors.",
     location: "Remote",
-    title: "Clinical Research Coordinator",
+    title: "Full Stack Engineer",
     url:
-      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
-  },
-  {
-    description:
-      "In order to advance Virufy’s technology, we need as much clinical data as possible from patients of multiple backgrounds and cultural identities in Latin America.",
-    location: "Remote",
-    title: "Clinical Outreach Member (Latin America)",
-    url:
-      "https://docs.google.com/document/d/1EZUFq9Y9xLFIaAB9NMHrICgyHkOyMcoUo9KzB99Qe4E/edit",
+      "https://docs.google.com/document/d/1au9s2wrUXlXx_tdK7q7EbB9xL0ZZ2NRdlDUgTwtjlW0/edit",
   },
 ];
