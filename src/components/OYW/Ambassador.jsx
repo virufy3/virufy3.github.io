1e8585b8d46134b12eda72a6f11293b96f0317c4
Yoonju Hwang <hwyj3464@users.noreply.github.com>
2021-04-10 01:01:23 -0400

    initial oyw

---
+++ src/components/OYW/Ambassador.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import GatsbyImage from "gatsby-image";
+import { useIntl } from "gatsby-plugin-intl";
+import { oyw } from "../../data/oyw";
+
+const Ambassador = (props) => {
+  const intl = useIntl();
+  const images = data.allFile.edges;
+
+  const {
+    image,
+    person: { id, defaultName, mediaLink },
+  } = props;
+
+  return (
+    <div className="flex flex-col text-left	 mx-auto mb-10 w-full">
+      <a className="w-full" target="_blank" rel="noreferrer" href={mediaLink}>
+        <GatsbyImage
+          className="mx-auto my-2 rounded-md overflow-hidden team-icons"
+          imgStyle={{ objectFit: "fill" }}
+          fluid={image}
+        />
+
+        <h5 className="mt-4 text-xl font-semibold text-black no-underline	">
+          {intl.formatMessage({
+            id: `OYW.ambassador.ambassador-list.${id}.name`,
+            defaultMessage: defaultName,
+          })}
+        </h5>
+      </a>
+      <div className="my-2"></div>
+      {intl.formatMessage({
+        id: `OYW.ambassador.ambassador-list.${id}.description`,
+      })}
+    </div>
+  );
+};
+
+export default ({ images }) => {
+  const intl = useIntl();
+  return (
+    <section className="p-8">
+      <div className="pb-12">
+        <h2 class="mb-4 text-4xl font-bold text-gray-200">
+          {intl.formatMessage({ id: "OYW.ambassador.header" })}
+        </h2>
+      </div>
+
+      <div className="mt-4 sm:grid sm:grid-cols-2 sm:gap-x-4 sm:gap-y-16 lg:grid-cols-3 lg:gap-x-6 xl:grid-cols-4">
+        {/* {oyw.map((amb, idx) => (
+          <Ambassador
+            key={`ambs${idx}`}
+            image={images[amb.imageName].childImageSharp.fluid}
+            person={amb}
+          />
+        ))} */}
+      </div>
+    </section>
+  );
+};
---
+++ src/pages/oyw.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import { useIntl } from "gatsby-plugin-intl";
+import { graphql } from "gatsby";
+import SEO from "../components/SEO";
+import GatsbyImage from "gatsby-image";
+import Ambassador from "../components/OYW/Ambassador";
+
+export const query = graphql`
+  {
+    allFile(
+      filter: { relativePath: { regex: "/images\\/oyw\\/.*\\.((png)|(jpg)|(jpeg))/" } }
+    ) {
+      edges {
+        node {
+          name
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function OYW({ data }) {
+  const intl = useIntl();
+  const images = {};
+
+  // add the images to an object to use in gatsby image
+  data.allFile.edges.forEach((edge) => (images[edge.node.name] = edge.node));
+
+  return (
+    <Layout>
+      <SEO title="OYW | Virufy" />
+      <div className="wrapper md:flex items-center justify-between md:py-4">
+        <div className="pb-10 md:pb-0 pr-6">
+          <h1 classNmae="font-bold text-4xl">
+            {intl.formatMessage({ id: "OYW.headers.header" })}
+          </h1>
+          <p
+            className="text-xl mt-4 md:w-8/12"
+            dangerouslySetInnerHTML={{
+              __html: intl.formatMessage({ id: "OYW.headers.sub-header" }),
+            }}
+          ></p>
+        </div>
+      </div>
+      <div className="my-10 wrapper">
+        <h2 className="mb-4 text-2xl font-bold text-gray-200">
+          {intl.formatMessage({ id: "OYW.section1.header" })}
+        </h2>
+        <p
+          className="text-lg mt-8"
+          dangerouslySetInnerHTML={{
+            __html: intl.formatMessage({ id: "OYW.section1.sub-header" }),
+          }}
+        ></p>
+
+        <section className="flex flex-wrap mb-20 wrapper md:justify-between">
+          <div className="flex flex-wrap justify-center my-5 md:w-1/2 md:my-10 md:flex-no-wrap md:justify-start">
+            <div>
+              <GatsbyImage
+                className="my-6 md:my-0 md:pr-2 md:w-3/12 mb-4"
+                fluid={images["globe"].childImageSharp.fluid}
+              />
+            </div>
+
+            <div className="w-full md:w-10/12">
+              <h2 className="mb-2 text-2xl font-bold text-gray-200">
+                {intl.formatMessage({ id: "OYW.section2.header" })}
+              </h2>
+              <p>{intl.formatMessage({ id: "OYW.section2.sub-header" })}</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap justify-center my-5 md:w-1/2 md:my-10 md:flex-no-wrap md:justify-start">
+            <div>
+              <GatsbyImage
+                className="my-6 md:my-0 md:pr-2 md:w-3/12 mb-4"
+                fluid={images["youth"].childImageSharp.fluid}
+              />
+            </div>
+            <div className="w-full md:w-10/12">
+              <h2 className="mb-2 text-2xl font-bold text-gray-200">
+                {intl.formatMessage({ id: "OYW.section2.youth" })}
+              </h2>
+              <p>{intl.formatMessage({ id: "OYW.section2.youth-des" })}</p>
+            </div>
+          </div>
+          <div class="flex flex-wrap justify-center my-5 md:w-1/2 md:my-10 md:flex-no-wrap md:justify-start">
+            <div className="w-full">
+              <GatsbyImage
+                className="my-6 md:my-0 md:pr-2 md:w-3/12 mb-4"
+                fluid={images["ambassador"].childImageSharp.fluid}
+              />
+            </div>
+            <div class="w-full md:w-10/12">
+              <h2 class="mb-2 text-2xl font-bold text-gray-200">
+                {intl.formatMessage({ id: "OYW.section2.ambassador" })}
+              </h2>
+              <p>{intl.formatMessage({ id: "OYW.section2.ambassador-des" })}</p>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap justify-center my-5 md:w-1/2 md:my-10 md:flex-no-wrap md:justify-start">
+            <div className="w-full">
+              <GatsbyImage
+                className="my-6 md:my-0 md:pr-2 md:w-3/12 mb-4"
+                fluid={images["diverse"].childImageSharp.fluid}
+              />
+            </div>
+            <div class="w-full md:w-10/12">
+              <h2 class="mb-2 text-2xl font-bold text-gray-200">
+                {intl.formatMessage({ id: "OYW.section2.diverse" })}
+              </h2>
+              <p>{intl.formatMessage({ id: "OYW.section2.diverse-des" })}</p>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      {/* Ambassador section */}
+      <Ambassador images={images} />
+    </Layout>
+  );
+}
