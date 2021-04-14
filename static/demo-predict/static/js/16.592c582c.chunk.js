d0318f6f7f908a5174e2469974347e4acdcf54c8
Maximiliano Duthey <mduthey@gmail.com>
2021-04-14 18:47:35 -0300

    Update demo with accessCode + Remove demo-predict

--- static/demo-predict/static/js/16.592c582c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppwa-demo"]=this["webpackJsonppwa-demo"]||[]).push([[16],{194:function(t,e,a){"use strict";a.r(e);var n=a(196);a.d(e,"default",(function(){return n.default}))}}]);
-//# sourceMappingURL=16.592c582c.chunk.js.map
