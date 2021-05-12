5d8908eab9c6d49d896ba402d13d16b657505a7a
Maximiliano Duthey <mduthey@gmail.com>
2021-05-12 17:48:42 -0300

    Update Study app

--- static/study/service-worker.js
+++ static/study/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://virufy.org/study/precache-manifest.53555f8587154efc910d1f4abd036a20.js"
+  "https://virufy.org/study/precache-manifest.0293d5a5c6513a504ee3cc619d4a2494.js"
 );
 
 self.addEventListener('message', (event) => {
