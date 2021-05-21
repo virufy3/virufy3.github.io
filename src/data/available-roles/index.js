90027ab6ee8adffd3dc296e4ef0d1f8378159817
Yoonju Hwang <hwyj3464@users.noreply.github.com>
2021-05-21 14:44:47 -0400

    update roles

--- src/data/available-roles/index.js
+++ src/data/available-roles/index.js
@@ -1,11 +1,11 @@
 export const roles = [
   {
     description:
-      "Seeking an individual to contribute on a part-time, volunteer basis to evaluate and screen potential candidates to fill vacant volunteering opportunities at Virufy. A key element of this position is the ability to attract highly-competitive and best-fit candidates for the job. Success in this role will be determined by providing an increased number of qualified and passionate candidates and retention rates.",
+      "Seeking an individual to contribute on a part-time volunteer basis and lead the North America ambassador team on the fundraising end to raise money for our global marketing and data crowdsourcing campaigns.",
     location: "Remote",
-    title: "Technical Recruiter (AI)",
+    title: "Fundraising Coordinator",
     url:
-      "https://docs.google.com/document/d/1umbOcHv3ZAMHIRx9XwWyAcFrdMlpdiimwJmwzKh_Jr0/edit#heading=h.sacgk1on2in4",
+      "https://docs.google.com/document/d/1OG3dc5acRJjEkys8kVp-NScttaNziQ1mdBWUIm6cnlY/edit",
   },
   {
     description:
@@ -17,19 +17,19 @@ export const roles = [
   },
   {
     description:
-      "Seeking an individual to contribute on a part-time volunteer basis and lead the North America ambassador team on the fundraising end to raise money for our global marketing and data crowdsourcing campaigns.",
+      "Seeking a part-time, volunteer HR Partner to work as part of the management team.",
     location: "Remote",
-    title: "Fundraising Coordinator",
+    title: "Human Resources Partner",
     url:
-      "https://docs.google.com/document/d/1OG3dc5acRJjEkys8kVp-NScttaNziQ1mdBWUIm6cnlY/edit",
+      "https://docs.google.com/document/d/124lK1uwh7fgaMD-J974jbLXO9iQEDl7pFiIbxigCcyI",
   },
   {
     description:
-      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
+      "Seeking an individual to serve as the first point of contact within Virufy for all HR-related inquiries.",
     location: "Remote",
-    title: "Clinical Research Coordinator",
+    title: "Human Resources Administrator",
     url:
-      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
+      "https://docs.google.com/document/d/191xws4_-ocyyMS9zTP0MEjsd2oXjGJqfNeVjyTaqepk/edit",
   },
   {
     description:
@@ -41,14 +41,6 @@ export const roles = [
   },
   {
     description:
-      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the Virufy development team, define the product roadmap, manage development progress and align with stakeholders.",
-    location: "Remote",
-    title: "Project / Product Manager",
-    url:
-      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
-  },
-  {
-    description:
       "Seeking an individual to contribute on a part-time volunteer basis as a Jack/Jill-of-all-trades!",
     location: "Remote",
     title: "Swiss Army Knife",
@@ -57,11 +49,59 @@ export const roles = [
   },
   {
     description:
-      "Seeking an individual to serve as the first point of contact within Virufy for all HR-related inquiries.",
+      "Seeking an individual to contribute on a part-time volunteer basis and be responsible for organizing clinical research trials.",
     location: "Remote",
-    title: "Human Resources Administrator",
+    title: "Global Clinical Research Coordinator",
     url:
-      "https://docs.google.com/document/d/191xws4_-ocyyMS9zTP0MEjsd2oXjGJqfNeVjyTaqepk/edit",
+      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time, volunteer basis to evaluate and screen potential candidates to fill vacant volunteering opportunities at Virufy. A key element of this position is the ability to attract highly-competitive and best-fit candidates for the job. Success in this role will be determined by providing an increased number of qualified and passionate candidates and retention rates.",
+    location: "Remote",
+    title: "Technical Recruiter (AI)",
+    url:
+      "https://docs.google.com/document/d/1umbOcHv3ZAMHIRx9XwWyAcFrdMlpdiimwJmwzKh_Jr0/edit#heading=h.sacgk1on2in4",
+  },
+  {
+    description:
+      "Seeking an individual to contribute on a part-time volunteer basis and collaborate closely with the Virufy development team, define the product roadmap, manage development progress and align with stakeholders.",
+    location: "Remote",
+    title: "Project / Product Manager",
+    url:
+      "https://docs.google.com/document/d/1x8NPeOzn6-IgCIrqxrK0zvcDY5858VJ_FRLGiewfWfY/edit",
+  },
+  {
+    description:
+      "Our team of part-time volunteers works with machine learning, deep learning, and signal analysis for audio analysis in a medical setting.",
+    location: "Remote",
+    title: "AI Engineer",
+    url:
+      "https://docs.google.com/document/d/10N9XSZXJkPelV4Ie-KHsCgOPf5LLNk00e9vit4Z-2IY",
+  },
+  {
+    description:
+      "Our team of part-time volunteers works with machine learning, deep learning, and signal analysis for audio analysis in a medical setting. We welcome individuals with professional experience in these domains to apply. The candidate will be immersed in a collaborative environment alongside AI engineers and mentors.",
+    location: "Remote",
+    title: "Director of Data Science",
+    url:
+      "https://docs.google.com/document/u/6/d/1cwcST8vefBJLaKjd_7Rj1bAcqV5rvyIcnwbJsEhfqIY/edit?ts=60a68f7a",
+  },
+  {
+    description:
+      "Seeking a volunteer website developer to collaborate with internal teams to redesign our website applications in a fast-paced environment. Our team of volunteers develop and maintain high quality web applications, which could be seen by hundreds of millions of people.",
+    location: "Remote",
+    title: "React Website Developer",
+    url:
+      "https://docs.google.com/document/u/4/d/12wKjLcrlQTLAVymKiZcuIHEHVXvcZsD5OxXX4jiV6_0/edit",
+  },
+  {
+    description:
+      "Seeking a part-time, volunteer film maker who is passionate about editing digital content and creating graphics/illustrations for outreach and marketing purposes.",
+    location: "Remote",
+    title: "Film Maker",
+    url:
+      "https://docs.google.com/document/d/1bgIGXD_LUUrbtszgHlOhdUnbojWfmMlSXNwnI1naDTc",
   },
   {
     description:
@@ -77,45 +117,5 @@ export const roles = [
     location: "Remote",
     title: "Product Marketeer",
     url: "https://virufy.org/apply",
-  },
-  {
-    description:
-      "Seeking a volunteer website developer to collaborate with internal teams to redesign our website applications in a fast-paced environment. Our team of volunteers develop and maintain high quality web applications, which could be seen by hundreds of millions of people.",
-    location: "Remote",
-    title: "React Website Developer",
-    url:
-      "https://docs.google.com/document/u/4/d/12wKjLcrlQTLAVymKiZcuIHEHVXvcZsD5OxXX4jiV6_0/edit",
-  },
-  {
-    description:
-      "Seeking a part-time, volunteer HR Partner to work as part of the management team.",
-    location: "Remote",
-    title: "Human Resources Partner",
-    url:
-      "https://docs.google.com/document/d/124lK1uwh7fgaMD-J974jbLXO9iQEDl7pFiIbxigCcyI",
-  },
-  {
-    description:
-      "Seeking a part-time, volunteer film maker who is passionate about editing digital content and creating graphics/illustrations for outreach and marketing purposes.",
-    location: "Remote",
-    title: "Film Maker",
-    url:
-      "https://docs.google.com/document/d/1bgIGXD_LUUrbtszgHlOhdUnbojWfmMlSXNwnI1naDTc",
-  },
-  {
-    description:
-      "Our team of part-time volunteers works with machine learning, deep learning, and signal analysis for audio analysis in a medical setting.",
-    location: "Remote",
-    title: "AI Engineer",
-    url:
-      "https://docs.google.com/document/d/10N9XSZXJkPelV4Ie-KHsCgOPf5LLNk00e9vit4Z-2IY",
-  },
-  {
-    description:
-      "Seeking a part-time volunteer to work with machine learning, deep learning, and signal processing for audio analysis in a medical setting. We welcome students and professionals with experience in these domains. The candidate will be immersed in a highly collaborative environment alongside senior AI engineers and mentors.",
-    location: "Remote",
-    title: "Full Stack Engineer",
-    url:
-      "https://docs.google.com/document/d/1au9s2wrUXlXx_tdK7q7EbB9xL0ZZ2NRdlDUgTwtjlW0/edit",
   },
 ];
