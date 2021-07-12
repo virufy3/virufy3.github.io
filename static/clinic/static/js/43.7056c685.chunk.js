9781e22f9a48f4da1ca2011172f3a7ba35e4a3f6
Maximiliano Duthey <mduthey@gmail.com>
2021-07-12 16:09:57 -0300

    Add clinic endpoint

---
+++ static/clinic/static/js/43.7056c685.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvirufy-study"]=this["webpackJsonpvirufy-study"]||[]).push([[43],{291:function(t,u,s){"use strict";s.r(u);var i=s(298);s.d(u,"default",(function(){return i.default}))}}]);
+//# sourceMappingURL=43.7056c685.chunk.js.map
